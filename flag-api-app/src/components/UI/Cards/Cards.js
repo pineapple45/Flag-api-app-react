84d7d44a95da55b65cc4b1dc61fb753e3587f98f
Gaurav Cherwal <gauravcherwal@outlook.com>
2020-09-19 10:39:10 +0530

    Changes after pulling

--- flag-api-app/src/components/UI/Cards/Cards.js
+++ flag-api-app/src/components/UI/Cards/Cards.js
@@ -8,24 +8,17 @@ class Cards extends Component {
   //   this.state = {
   //     countries: [],
   //   };
-<<<<<<< HEAD
   // }
 
   // const fs = () => {
   //     setState({
 
   //     })
-=======
->>>>>>> 36bfb5ce91935173d6ad9d39f23558a8ba8b6778
   // }
 
   //http get request will be handled here
   // componentDidMount() {
-<<<<<<< HEAD
   //   // console.log("CARDS CALLED");
-=======
-  //   console.log("CARDS CALLED");
->>>>>>> 36bfb5ce91935173d6ad9d39f23558a8ba8b6778
   //   fetch("https://restcountries.eu/rest/v2/all")
   //     .then((res) => res.json())
   //     .then((data) => {
